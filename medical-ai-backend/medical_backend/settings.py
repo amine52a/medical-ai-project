4b20ac3f4d8f32235305ab0a362b66315f3399d2
Mohamed amine abdellah <aminebenmohamedabdellah@gmail.com>
2025-08-20 02:21:56 +0100

    Configure STATIC_ROOT for Azure

--- medical-ai-backend/medical_backend/settings.py
+++ medical-ai-backend/medical_backend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -150,7 +151,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -168,3 +169,4 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # settings.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
